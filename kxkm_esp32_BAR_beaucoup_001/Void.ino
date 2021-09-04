c85f54ffa722216ca9ba142eeee3c2b39883433c
richard fontaine <ririfonfon10@gmail.com>
2021-09-04 19:06:16 +0200

    delete

--- kxkm_esp32_BAR_beaucoup_001/Void.ino
+++
@@ -1,6 +0,0 @@
-void Black() {
-  for (int i = 0 ; i < NUM_LEDS_PER_STRIP ; i++) {
-    strands[0]->pixels[i] = pixelFromRGBW(0, 0, 0, 0);
-    strands[1]->pixels[i] = pixelFromRGBW(0, 0, 0, 0);
-  }//for i
-}
